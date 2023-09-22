37068f453bd54a714ec45ca1a528187da34cf197
abdullah <98279119+AbdullahB8mi@users.noreply.github.com>
2023-09-22 18:40:27 +0300

    Add files via upload

--- temp/RandomGroups/src/Sorter.java
+++ temp/RandomGroups/src/Sorter.java
@@ -24,7 +24,7 @@ public class Sorter {
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
-            
+
             // Move elements of arr[0..i-1], that are greater than key,
             // to one position ahead of their current position
             while (j >= 0 && arr[j] > key) {
@@ -34,8 +34,10 @@ public class Sorter {
             arr[j + 1] = key;
         }
 
-        public static void sort(int[] arr) {
-            insertionSort(arr);
-        }
+    }
+    public static void sort(int[] arr) {
+        selectionSort(arr);
+        insertionSort(arr);
+    }
 }
 
